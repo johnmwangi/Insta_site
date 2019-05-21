200dedeb3251a80ad36c563925229462f1f84730
johnmwangi <jonesmwas356@gmail.com>
2019-05-21 14:21:38 +0300

    DEploy

--- Instagram/settings.py
+++ Instagram/settings.py
@@ -121,13 +121,6 @@ WSGI_APPLICATION = 'Instagram.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
